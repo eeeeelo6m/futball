34c97de84eb6fd236c20057cbda5e6b52d22939c
agd30 <agd30072010@gmail.com>
2021-02-23 16:30:35 +0300

    сделан авто пилот правого игрока,сделана система голов,управление левым игроком.

--- futball.py
+++ futball.py
@@ -1,16 +1,20 @@
-import pygame, time
+import pygame, time,random
 from pygame import display, draw, event, key
+pygame.init()
 c=pygame.time.Clock()
 vorota_left = (0, 0, 20, 680)
 vorota_right = (1180, 0, 20, 680)
 
-screen = display.set_mode([1200, 680])
-player_right = pygame.Rect(1000, 300, 1, 300)
-player_left=pygame.Rect(200,300,1,300)
+timer=event.custom_type()
 dvigenie = pygame.Rect([500, 320, 100, 100])
+screen = display.set_mode([1200, 680])
+player_right = pygame.Rect(1000, dvigenie.centery, 3, 150)
+player_left=pygame.Rect(200,300,3,150)
+
 speedx = -10
 speedy = 5
 while 1 == 1:
+
     c.tick(100)
     print(c.get_fps())
 
@@ -19,12 +23,22 @@ while 1 == 1:
     for r in e:
         if r.type == pygame.QUIT:
             exit()
-
+        if r.type==timer:
+            speedx=random.randint(-5,5)
+            speedy=random.randint(-5,5)
+            if speedx==0:
+                speedx = random.randint(-5, 5)
+            if speedy==0:
+                speedy = random.randint(-5, 5)
     keys = key.get_pressed()
+    # if keys[pygame.K_UP]:
+    #     player_right.y += -5
+    # if keys[pygame.K_DOWN]:
+    #     player_right.y +=5
     if keys[pygame.K_w]:
-        player_right.y += -10
+        player_left.y-=5
     if keys[pygame.K_s]:
-        player_right.y +=10
+        player_left.y+=5
     # движение
 
     dvigenie.x += speedx
@@ -32,25 +46,31 @@ while 1 == 1:
     t = player_right.colliderect(dvigenie)
     if t == 1:
         if speedx < 0:
-            speedx = 20
+            speedx = random.randint(5,10)
             dvigenie.left = player_right.right
 
         elif speedx > 0:
-            speedx = -30
+            speedx = random.randint(-10,-5)
             dvigenie.right = player_right.left
     if v==1:
         if speedx<0:
-            speedx=20
+            speedx=random.randint(5,10)
             dvigenie.left=player_left.right
         elif speedx>0:
-            speedx=-20
+            speedx=random.randint(-10,-5)
             dvigenie.right=player_left.left
     if 1180 < dvigenie.right:
-        dvigenie.right = 1180
-        speedx = -10
+        dvigenie.centerx = 600
+        dvigenie.centery=340
+        pygame.time.set_timer(timer,1500,True)
+        speedx = 0
+        speedy=0
     if 19 > dvigenie.x:
-        dvigenie.x = 20
-        speedx = 5
+        dvigenie.centerx = 600
+        dvigenie.centery=340
+        pygame.time.set_timer(timer,1500,True)
+        speedx = 0
+        speedy=0
 
     dvigenie.y += speedy
     if 0 > dvigenie.y:
@@ -60,7 +80,7 @@ while 1 == 1:
     if 680 < dvigenie.bottom:
         speedy = -10
         dvigenie.bottom = 680
-
+    player_right.y=dvigenie.y
     # рисование
     screen.fill([20, 30, 255])
     draw.rect(screen, [30, 200, 255], vorota_left)
